fd1c2df069194ce1398b46c993a58680e85c5cee
jayanihardik <jayanihardik@gmail.com>
2021-03-30 15:30:26 +0530

    - added function component in HOC

--- src/Components/HOC/Hoc.js
+++ src/Components/HOC/Hoc.js
@@ -1,35 +1,57 @@
 import React from 'react'
 
 const Hoc = (Component) => {
-    return class extends React.Component {
-        constructor() {
-            super()
-            this.state = {
-                counter: 0
+
+    const NewComponent = () => {
+        const [counter, setCount] = useState(0)
+
+        const handelIncrement = () => {
+            setCount(counter + 1)
+        }
+
+        const handelDecrement = () => {
+            if (counter > 0) {
+                setCount(counter - 1)
             }
         }
 
-        handelIncrement = () => {
-            this.setState({
-                counter: this.state.counter + 1
-            })
-        }
+        return <Component counter={counter} handelIncrement={handelIncrement} handelDecrement={handelDecrement} />
 
-        handelDecrement = () => {
-            if (this.state.counter > 0) {
-                this.setState({
-                    counter: this.state.counter - 1
-                })
-            }
-        }
-
-        render() {
-            return <div className="d-flex">
-                <Component counter={this.state.counter} handelIncrement={this.handelIncrement} handelDecrement={this.handelDecrement} />
-            </div>
-        }
     }
+    return NewComponent;
 }
+
 export default Hoc;
 
 
+// const Hoc = (Component) => {
+//     return class extends React.Component {
+//         constructor() {
+//             super()
+//             this.state = {
+//                 counter: 0
+//             }
+//         }
+
+//         handelIncrement = () => {
+//             this.setState({
+//                 counter: this.state.counter + 1
+//             })
+//         }
+
+//         handelDecrement = () => {
+//             if (this.state.counter > 0) {
+//                 this.setState({
+//                     counter: this.state.counter - 1
+//                 })
+//             }
+//         }
+
+//         render() {
+//             return <div className="d-flex">
+//                 <Component counter={this.state.counter} handelIncrement={this.handelIncrement} handelDecrement={this.handelDecrement} />
+//             </div>
+//         }
+//     }
+// }
+// export default Hoc;
